a3a039081a56abb0c1482438bb3ee9f9e8d4d84f
Patrik Drbal <drbalpat@fit.cvut.cz>
2021-05-09 19:34:27 +0200

    [semestral] Update pack.sh and prohlaseni.txt

--- semestral/pack.sh
+++ semestral/pack.sh
@@ -1,1 +1,13 @@
+#! /bin/bash
 
+F='drbalpat'
+
+zip -r drbalpat.zip       \
+       $F/zadani.txt      \
+       $F/prohlaseni.txt  \
+       $F/Makefile        \
+       $F/src/\*          \
+       $F/doc/\*          \
+       $F/examples/\*     \
+       $F/Doxyfile        \
+       $F/assets/\*       \
